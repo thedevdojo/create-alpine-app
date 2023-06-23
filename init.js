b94edcc4062b3288ae78b33b81274dfeac922e23
Tony Lea <tnylea@gmail.com>
2023-06-23 13:27:08 -0400

    Adding initialize function which will add the module type to the json file

---
+++ init.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+console.log(__dirname);
+let updatedJSON = null;
+
+fs.readFile(__dirname + '/package.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    let object = JSON.parse(data);
+    object['type']  = 'module';
+    updatedJSON = JSON.stringify(object, null, 2);
+
+    if(updatedJSON !== null) {
+      console.log('writing');
+      fs.writeFile(__dirname + '/package.json', updatedJSON, err => {
+        if (err) {
+          console.error(err);
+        }
+        // file written successfully
+      });
+    }
+
+  });
+
+
