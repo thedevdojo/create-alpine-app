98d1968a200a58b1839028a5986fde710caff623
Tony Lea <tnylea@gmail.com>
2023-06-23 13:09:01 -0400

    Adding latest content

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,28 @@
 #! /usr/bin/env node
-console.log("Hello, World!");
+
+const {execSync} = require('child_process');
+
+const runCommand = command => {
+
+    try {
+        execSync (`${command}`, {stdio: 'inherit'});
+    } catch (e) {
+        console.error (`Failed to execute ${command}`, e);
+        return false;
+    }
+    
+    return true;
+}
+
+const repoName = process.argv[2];
+
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/thedevdojo/create-alpine-app ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console. log(`Cloning the repository with name ${repoName}`);
+const checkedOut = runCommand (gitCheckoutCommand);
+
+if(!checkedOut) process.exit(1);
+
+console.log("Congratulations! You are ready. Follow the following commands to start");
+console.log(`cd ${repoName} && npm run dev`);
